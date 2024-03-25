c78ec050a20684566bf0bbcf3efd586e651f883d
Kris Simon <kris@ausdertechnik.de>
2024-03-25 17:01:50 +0100

    Fix path to expected file

--- run-tests.sh
+++ run-tests.sh
@@ -4,11 +4,31 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p "${SCRIPT_DIR}/results"
 
+ErrCount=0
+function err(){
+  local msg
+  msg=${1}
+
+  echo ${msg}
+  ErrCount=$(expr ${ErrCount} + 1)
+}
+
+rm -f "${SCRIPT_DIR}/results/.env.simple.result"
 expect "${SCRIPT_DIR}"/test.only-defaults.expect
-cmp -s "${SCRIPT_DIR}/samples/.env.simple.sample.expected" "${SCRIPT_DIR}./results/.env.simple.result" \
-|| echo "Generated file ${SCRIPT_DIR}./results/.env.simple.result differs from expected result."
+cmp -s "${SCRIPT_DIR}/samples/.env.simple.sample.expected" "${SCRIPT_DIR}/results/.env.simple.result" \
+|| echo "Generated file ${SCRIPT_DIR}/results/.env.simple.result differs from expected result."
 
+rm -f "${SCRIPT_DIR}/results/.env.with-comments.result"
 expect "${SCRIPT_DIR}"/test.with-comments.expect
-cmp -s "${SCRIPT_DIR}/samples/.env.with-comments.sample.expected" "${SCRIPT_DIR}./results/.env.with-comments.result" \
-|| echo "Generated file ${SCRIPT_DIR}./results/.env.with-comments.result differs from expected result."
+cmp -s "${SCRIPT_DIR}/samples/.env.with-comments.sample.expected" "${SCRIPT_DIR}/results/.env.with-comments.result" \
+|| echo "Generated file ${SCRIPT_DIR}/results/.env.with-comments.result differs from expected result."
 
+
+echo $'\n'${ErrCount} 'Errors.'
+if [ ${ErrCount} -eq 0 ]; then
+  echo "All tests passed."
+  exit 0
+else
+  echo "Tests failed."
+  exit 1
+fi
